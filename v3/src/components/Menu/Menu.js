fe1e3fa7e051a85163ed87cc3d3522741052ebb3
Vlad Puscaru <vlad.cpuscaru@gmail.com>
2024-01-01 12:38:45 +0100

    v3: Menu mobile

--- v3/src/components/Menu/Menu.js
+++ v3/src/components/Menu/Menu.js
@@ -1,4 +1,9 @@
 import styles from "./Menu.module.sass"
+import iconFacebook from "../../common/images/icons8-facebook.svg";
+import iconTwitter from "../../common/images/icons8-twitterx.svg";
+import iconLinkedin from "../../common/images/icons8-linkedin.svg";
+import iconGithub from "../../common/images/icons8-github.svg";
+import { ReactSVG } from "react-svg";
 
 const Menu = ({activeSection, onMenuItemClick}) => {
     return (
@@ -17,16 +22,31 @@ const Menu = ({activeSection, onMenuItemClick}) => {
                 <div className={styles.shape}></div>
                 <a href="#">resume</a>
             </div>
-            <div onClick={() => onMenuItemClick(3)}
-                 className={`${styles.cell} ${activeSection === 3 ? styles.active : ""}`}>
+            <div className={styles.cell}>
                 <div className={styles.shape2}></div>
                 <div className={styles.shape}></div>
-                <a href="#">contact</a>
+                <a href="#">
+                    <ReactSVG src={iconFacebook}/>
+                </a>
+                <a href="#">
+                    <ReactSVG src={iconTwitter}/>
+                </a>
+                <a href="#">
+                    <ReactSVG src={iconLinkedin}/>
+                </a>
+                <a href="#">
+                    <ReactSVG src={iconGithub}/>
+                </a>
             </div>
             <div onClick={() => onMenuItemClick(-1)}
                  className={`${styles.back} ${activeSection !== -1 ? styles.active : ""}`}>
                 <a href="#">x</a>
             </div>
+
+            <div className={styles.shapeMobileTop}></div>
+            <div className={styles.shapeMobileRight}></div>
+            <div className={styles.shapeMobileBottom}></div>
+            <div className={styles.shapeMobileLeft}></div>
         </nav>
     )
 }
